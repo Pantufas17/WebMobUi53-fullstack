5c2aa4c4cb1e0b10cc15f94efe1c8c05ea005f0a
Pantufas17 <nunoamarofaria@hotmail.com>
2026-05-14 11:54:54 +0200

    ajout du #create dans les routes pour un nouveau sondage

--- resources/js/stores/routes.js
+++ resources/js/stores/routes.js
@@ -1,5 +1,7 @@
 import DashboardPage from "../pages/DashboardPage.vue";
+import PollEditorPage from "../pages/PollEditorPage.vue";
 
 export const routes = [
     { hash: "#dashboard", label: "Mes sondages", component: DashboardPage },
+    { hash: "#create", label: "Nouveau sondage", component: PollEditorPage },
 ];
