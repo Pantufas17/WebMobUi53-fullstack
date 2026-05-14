de2e2e8cc832bdc14393b3e72ada0eaed72dc78f
Pantufas17 <nunoamarofaria@hotmail.com>
2026-05-14 10:32:49 +0200

    creation du component TheHeader.vue

---
+++ resources/js/components/TheHeader.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+    title: { type: String, default: "Sondages" },
+});
+</script>
+
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+    background-color: #1e1b4b;
+    color: white;
+}
+h1 {
+    margin: 0;
+    font-size: 1.25rem;
+}
+</style>
