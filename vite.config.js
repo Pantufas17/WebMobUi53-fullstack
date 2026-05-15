d32eeaddd170e0a2db674f523b0ed3ef9fe912e5
Pantufas17 <nunoamarofaria@hotmail.com>
2026-05-15 11:03:50 +0200

    maj vite.config pour nouveau entrepoint

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         vue(),
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/poll-dashboard.js',
-                'resources/js/poll-dashboard-integrated.js',
+                "resources/css/app.css",
+                "resources/js/poll-dashboard.js",
+                "resources/js/poll-dashboard-integrated.js",
+                "resources/js/poll-viewer.js",
             ],
             refresh: true,
         }),
@@ -19,10 +20,10 @@ export default defineConfig({
     server: {
         host: true,
         hmr: {
-            host: 'localhost'
+            host: "localhost",
         },
         watch: {
-            ignored: ['**/storage/framework/views/**'],
+            ignored: ["**/storage/framework/views/**"],
         },
     },
 });
