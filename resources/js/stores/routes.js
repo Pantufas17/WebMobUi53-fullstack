c0c0ec946df97e9208f77fffc0f7c12909465d61
Pantufas17 <nunoamarofaria@hotmail.com>
2026-05-15 10:12:09 +0200

    ajout du hash de edit dans stores/routes.js  pour modifier du coup un sondage

--- resources/js/stores/routes.js
+++ resources/js/stores/routes.js
@@ -4,4 +4,5 @@ import PollEditorPage from "../pages/PollEditorPage.vue";
 export const routes = [
     { hash: "#dashboard", label: "Mes sondages", component: DashboardPage },
     { hash: "#create", label: "Nouveau sondage", component: PollEditorPage },
+    { hash: "#edit", label: "Modifier", component: PollEditorPage },
 ];
